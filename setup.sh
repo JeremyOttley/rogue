1762d1f2ea5baab823d6675f2774ee15794bc4f2
Jeremy Ottley <Gazaunga@gmail.com>
2019-01-14 07:36:43 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
-git clone git@github.com:lepisma/rogue ~/.emacs.d/private/rogue
+git clone git@github.com:jeremyottley/rogue ~/.emacs.d/private/rogue
 ln -s ~/.emacs.d/private/rogue/.spacemacs ~/.spacemacs
 touch ~/.emacs-custom.el
